671e041d0d36357e7433e78233a4682fbfc795d6
McClelland Kemp <McClellandLegge@users.noreply.github.com>
2018-02-14 10:49:57 -0600

    updating description of top level app workings

--- inst/demo_app/app.R
+++ inst/demo_app/app.R
@@ -1,19 +1,25 @@
 
 library("shiny")
-library("magrittr")
+library("purrr")
 library("shinydashboard")
 library("shinytabconstructor")
 
+# create the UI by sourcing all the nested files below the ui/ directory,
+# package functions handle the list of UI objects appropriately in the
+# menu sidebar
 ui     <- includeR("ui")
 
-server <- function(input, output, session) { 
-  
+# create the server function by sourcing all the scripts below the server/
+# directory which are then returned as a list, NULLs (empty scripts) are then
+# tossed and the rest evaluated to be passed to the top level function
+server <- function(input, output, session) {
+
   includeR("server", recursive = TRUE) %>%
     Filter(Negate(is.null), .) %>%
     do.call(eval, .)
-    
-  
+
 } #/ serverFunction
 
+# finally, run the app
 shinyApp(ui = ui, server = server)
 
