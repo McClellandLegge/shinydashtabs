4563d9c05325f83d549fc4f08ed1dbfef4395d60
McClelland Legge <McClellandLegge@users.noreply.github.com>
2018-03-01 14:51:43 -0600

    a better ui for the example tabs

--- inst/templates/new_tab.R
+++ inst/templates/new_tab.R
@@ -2,8 +2,19 @@ tab_name <- thisTabName()
 
 tabItem(
   tabName = tab_name,
-  h2(tab_name),
-  actionButton(paste0(tab_name, "_button"), "Toggle"),
-  plotOutput(paste0(tab_name, "_plot"))
+  fluidRow(
+    column(width = 6,
+      box(title = sprintf("%s Controls", tools::toTitleCase(tab_name)), width = "100%",
+          solidHeader = TRUE, status = "warning",
+        actionButton(paste0(tab_name, "_button"), "Toggle")
+      ) #/ box
+    ), #/ column 1
+    column(width = 6,
+      box(title = "Scatterplot", status = "primary", width = "100%",
+          solidHeader = TRUE,
+          plotOutput(paste0(tab_name, "_plot"))
+      ) #/ BOX
+    ) #/ column 2
+  ) #/ fluidRow
 ) #/ tabItem
 
