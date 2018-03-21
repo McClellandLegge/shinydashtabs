ec4f7b11bb23bca0d41b5a762bcc3727dc2d33b1
McClelland Legge <McClelland.Legge@gmail.com>
2018-03-21 10:08:09 -0500

    fix #2 correctly... changing api, removing server function from namespace in order to allow any packages to be used without needed to be imported by the package

--- inst/demo_app/app.R
+++ inst/demo_app/app.R
@@ -12,4 +12,16 @@ sourceR("init")
 # create the UI by sourcing all the nested files below the ui/ directory,
 # package functions handle the list of UI objects appropriately in the
 # menu sidebar. use the server function that sources all serverside files
-shinyApp(ui = includeR("ui"), server = serverFunc)
+shinyApp(ui = includeR("ui"), server = function(input, output, session) {
+
+   server_files <- list.files(
+      path       = "./server/"
+    , recursive  = TRUE
+    , pattern    = "(*reactives.R|*observers.R|*renders.R)"
+    , full.names = TRUE
+  )
+
+  for (file in server_files) {
+    source(file, local = TRUE)$value
+  }
+})
