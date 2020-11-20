b9d07e7de405c75f1f750dc4f5f9f57d73758088
McClelland Legge <mlegge@boodle.imperatoris.net>
2020-11-20 16:08:33 -0600

    fixing some formatting in the new_tab.R

--- inst/templates/new_tab.R
+++ inst/templates/new_tab.R
@@ -4,16 +4,23 @@ tabItem(
   tabName = tab_name,
   fluidRow(
     column(width = 6,
-      box(title = sprintf("%s Controls", tools::toTitleCase(tab_name)), width = "100%",
-          solidHeader = TRUE, status = "warning",
+      box(
+        title       = sprintf("%s Controls", tools::toTitleCase(tab_name)),
+        status      = "warning",
+        width       = "100%",
+        solidHeader = TRUE,
         actionButton(paste0(tab_name, "_button"), "Toggle")
       ) #/ box
     ), #/ column 1
-    column(width = 6,
-      box(title = "Scatterplot", status = "primary", width = "100%",
-          solidHeader = TRUE,
-          plotOutput(paste0(tab_name, "_plot"))
-      ) #/ BOX
+    column(
+      width = 6,
+      box(
+        title       = "Scatterplot",
+        status      = "primary",
+        width       = "100%",
+        solidHeader = TRUE,
+        plotOutput(paste0(tab_name, "_plot"))
+      ) #/ box
     ) #/ column 2
   ) #/ fluidRow
 ) #/ tabItem
